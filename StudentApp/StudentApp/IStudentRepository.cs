50ce32209aa5c5b9523b7a56880279bb0546e96e
zara134 <liverpulovac11@gmail.com>
2026-04-28 15:23:09 +0200

    add student and student service

---
+++ StudentApp/StudentApp/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApp
+{
+    public interface IStudentRepository
+    {
+        Student GetStudentById(int id);
+        void Save(Student student);
+    }
+}
---
+++ StudentApp/StudentApp/Student.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+namespace StudentApp;
+
+public class Student
+{
+    private string? name;
+    public string? Name
+    {
+        get { return name; }
+    }
+    private int points;
+    public int Points => points;
+
+    public Student(string Name)
+    {
+        name = Name;
+    }
+    public Student() { }
+    public string? SayMyName()
+    {
+        return Name;
+    }
+    public void Learn(int hours)
+    {
+        points += hours * 10;
+    }
+    public bool IsTestPassed()
+    {
+        if (points > 50)
+            return true;
+        else
+            return false;
+    }
+}
